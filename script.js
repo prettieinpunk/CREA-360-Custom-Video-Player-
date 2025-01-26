650f2372097284fabc21db1743e402e48ef89075
prettieinpunk <160554112+prettieinpunk@users.noreply.github.com>
2025-01-26 17:58:21 -0500

    Implement video player functionality

---
+++ script.js
@@ -0,0 +1,37 @@
+// Selectors
+const video = document.getElementById("video");
+const play = document.getElementById("play");
+const stop = document.getElementById("stop");
+const progress = document.getElementById("progress");
+const timestamp = document.getElementById("timestamp");
+
+// Event Listeners
+video.addEventListener("click", toggleVideoStatus);
+video.addEventListener("pause", updatePlayIcon);
+video.addEventListener("play", updatePlayIcon);
+video.addEventListener("timeupdate", updateProgress);
+
+play.addEventListener('click', toggleVideoStatus);
+stop.addEventListener('click', stopVideo);
+progress.addEventListener('change', setVideoProgress);
+
+// Handlers
+// Play and Pause Video
+function toggleVideoStatus() {
+    if (video.paused) {
+      video.play();
+    } else {
+      video.pause();
+    }
+}
+
+// Update the Play/Pause Icon
+function updatePlayIcon() {
+    if (video.paused) {
+      play.innerHTML = '<i class="fa fa-play fa-2x"></i>';
+    } else {
+      play.innerHTML = '<i class="fa fa-pause fa-2x"></i>';
+    }
+}
+
+
